ea35532fecf565a6f4aca18b0bb3d174beba1d71
Atul <kkmonlee@gmail.com>
2017-04-20 02:56:40 +0100

    O(npermutations

---
+++ Mathematics/permutations.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long lld;
+
+int n;
+int niz[100];
+bool inPerm[100];
+int currPerm[100];
+
+void generatePermutations(int pos) {
+    if (pos == n) {
+        for (int i = 0; i < n; i++)
+            printf("%d ", currPerm[i]);
+        printf("\n");
+    }
+    for (int i = 0; i < n; i++) {
+        if (!inPerm[i]) {
+            currPerm[pos] = niz[i];
+            inPerm[i] = true;
+            generatePermutations(pos + 1);
+            inPerm[i] = false;
+        }
+    }
+}
+
+int main() {
+    n = 3;
+    niz[0] = 1;
+    niz[1] = 2;
+    niz[2] = 3;
+    // generatePermutations(0);
+
+    return 0;
+}
